a800707a216493919ca6fb67a459ec786b8f15d7
Bahar <fouladpourib1447@my.sduhsd.com>
2020-01-23 11:45:15 -0800

    update makefile and c file

--- project.c
+++ project.c
@@ -5,8 +5,6 @@
 //source: programmingsimplified.com/c-program-generate-random-numbers
 double thirdTriangleSide (int x, int y);
 double perimeterofTriangle (int x, int y, double ts);
-void perimeterofRectangle (int x, int y);
-void product (int x, int y);
 
 int randomfunc (int low, int high, int count)
 {
@@ -29,14 +27,32 @@ int main()
   printf("height = %d \nbase = %d\n", a, b);
   if (a <= 25 && b<= 25)
     {
+      printf("Because both values were less than 25, triangle-based calculations were done.\n");
       double ts = thirdTriangleSide (a, b);
       perimeterofTriangle (a, b, ts);
+      if (b <= 12)
+      {
+	printf("A base value of 12 or less prints looped area calculations.\n");
+	int z;
+        for (z = 1; z <= b; z++)
+	{
+	  printf("Area of the triangle with base = %d is %d.\n", z, a*z/2);
+	}
+      }
+      else
+      {
       printf("Area of the triangle is %d.\n", a*b/2);
+      }
     }
   else
     {
-      perimeterofRectangle (a, b);
-      product (a, b);
+      printf("Because one or both number(s) were above 25, rectangle-based calculations were done.\n");
+      int pr;
+      pr = 2*a + 2*b;
+      printf("Perimeter of the rectange is %d.\n", pr);
+      int p;
+      p = a*b;
+      printf("Area of the rectangle is %d.\n", p);
     }
   return 0;
 }
@@ -47,22 +63,10 @@ double thirdTriangleSide(int x, int y)
   c = pow(x, 2) + pow(y, 2) - (2*x*y*cos(90));
   th = sqrt(c);
   printf("The third side of the triangle is %f.\n", th);
-  return 0;
+  return th;
 }
 double perimeterofTriangle (int x, int y, double ts)
 {
   printf("Perimeter of the triangle is %f.\n", x + y + ts);
   return 0;
-}
-void perimeterofRectangle (int x, int y)
-{
-  int pr;
-  pr = 2*x + 2*y;
-  printf("Perimeter of the rectangle is %d.\n", pr);
-}
-void product (int x, int y)
-{
-  int p;
-  p = x*y;
-  printf("Area of the rectangle is %d.\n", p);
 }
